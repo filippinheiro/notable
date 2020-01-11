9cf5d8b1296c1d8451b9662f35433f70d1c71338
filippinheiro <filippinheiro1@gmail.com>
2020-01-11 16:03:48 -0300

    feature add : added note details

--- src/pages/NoteDetail.js
+++ src/pages/NoteDetail.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-export default function NoteDetail({navigation: {state: {params: {note}}}}) {
+export default function NoteDetail({ navigation: { state: { params: { note } } } }) {
     return (
-        <View style = {StyleSheet.container}>
-        <Text>{note.content}</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>{note.title}</Text>
+            <Text style = {styles.content}>{note.content}</Text>
         </View>
     )
 }
 
-
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        paddingBottom: 15,
+        paddingTop: 15,
+    },
+    container: {
+        padding: 10
+    },
+    content: {
+        fontSize: 18,
+        paddingStart: 15,
+        paddingLeft: 15,
+    }
+})
 NoteDetail.navigationOptions = {
     title: 'Notable'
 } 
